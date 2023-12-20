73f23e0bbe5c4b33b1b8a3e2471451720dc4cab5
GabrielLimaDeSouza <gabriel.souza.1354648@sga.pucminas.br>
2023-12-20 19:51:31 -0300

    fix: banner imgs bug

--- frontend/smartfit-frontend-challenge/app/components/information-banner.tsx
+++ frontend/smartfit-frontend-challenge/app/components/information-banner.tsx
@@ -4,12 +4,38 @@ export default function InformationBanner() {
   return (
     <div className="w-full bg-background-gray px-2 py-4 flex flex-wrap 2xl:flex-nowrap gap-4 justify-between mb-12">
       <div className="w-full flex gap-4 justify-between">
-        <InformationCard title="Máscara" img1="required-mask.png" img2="recommended-mask.png" desc1="Obrigatório" desc2="Recomendado"/>
-        <InformationCard title="Toalha" img1="required-towel.png" img2="recommended-towel.png" desc1="Obrigatório" desc2="Recomendado"/>
+        <InformationCard
+          title="Máscara"
+          img1="required-mask.png"
+          img2="recommended-mask.png"
+          desc1="Obrigatório"
+          desc2="Recomendado"
+        />
+        <InformationCard
+          title="Toalha"
+          img1="required-towel.png"
+          img2="recommended-towel.png"
+          desc1="Obrigatório"
+          desc2="Recomendado"
+        />
       </div>
       <div className="w-full flex gap-4 justify-between">
-        <InformationCard title="Bebedouro" img1="partial-fountain.png" img2="forbidden-fountain.png" desc1="Parcial" desc2="Proibido"/>
-        <InformationCard title="Vestiários" img1="required-lockerroom.png" img2="partial-lockerroom.png" img3="forbidden-lockerroom.png" desc1="Liberado" desc2="Parcial" desc3="Fechado" />
+        <InformationCard
+          title="Bebedouro"
+          img1="partial-fountain.png"
+          img2="not_allowed-fountain.png"
+          desc1="Parcial"
+          desc2="Proibido"
+        />
+        <InformationCard
+          title="Vestiários"
+          img1="allowed-lockerroom.png"
+          img2="partial-lockerroom.png"
+          img3="closed-lockerroom.png"
+          desc1="Liberado"
+          desc2="Parcial"
+          desc3="Fechado"
+        />
       </div>
     </div>
   );
