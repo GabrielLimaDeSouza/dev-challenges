6b0e62c64de532deb0d1491bcb0305e52f3bb709
GabrielLimaDeSouza <gabriel.souza.1354648@sga.pucminas.br>
2024-02-03 21:57:38 -0300

    feat: initial commit

---
+++ backend/picpay-challenge/src/test/java/com/gabriellima/picpaychallenge/PicpayChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gabriellima.picpaychallenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PicpayChallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
